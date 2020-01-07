f786326167b8282e412c5fb1bf4158791ce3bddb
victor <victor-gr7@hotmail.com>
2020-01-07 16:46:26 +0100

    task 9/01/2020

--- example/create_documents.py
+++ example/create_documents.py
@@ -17,6 +17,16 @@ def create_document(doc, emb, index_name):
         'title': doc['title'],
         'text_vector': emb
     }
+"""def load_dataset_5(path):
+    docs = []
+    count= 0
+    data = parse_data(path)
+    for row in data:
+        doc = {
+            'personaId': count
+            'sentenceId':
+            'sentence': ' '.join(row['persona_info'])
+        }"""
 def load_dataset_4(path):
     docs = []
     count = 0
@@ -29,6 +39,47 @@ def load_dataset_4(path):
         docs.append(doc)
         count += 1
     return docs
+def parse_data_2(path):
+    docs = []
+    with open(path, 'r', encoding='utf-8') as file:
+        data = []
+        sentence_counter = 0
+        persona_counter = 0
+        for line in file.readlines():
+            line = line.strip()
+
+            if len(line) == 0:
+                continue
+
+            space_idx = line.find(' ')
+            if space_idx == -1:
+                dialog_idx = int(line)
+            else:
+                dialog_idx = int(line[:space_idx])
+
+            if int(dialog_idx) == 1:
+                persona_counter += 1
+                data.append({'persona_info': [], 'dialog': []})
+
+            dialog_line = line[space_idx + 1:].split('\t')
+            dialog_line = [l.strip() for l in dialog_line]
+
+            if dialog_line[0].startswith('your persona:'):
+                persona_info = dialog_line[0].replace('your persona: ', '')
+                data[-1]['persona_info'].append(persona_info)
+                doc = {
+                    'personaId':persona_counter,
+                    'sentenceId':sentence_counter,
+                    'sentence':persona_info
+                }
+                docs.append(doc)
+
+            elif len(dialog_line) > 1:
+                data[-1]['dialog'].append(dialog_line[0])
+                data[-1]['dialog'].append(dialog_line[1])
+            sentence_counter += 1
+        return data
+
 def parse_data(path):
     with open(path, 'r', encoding='utf-8') as file:
         data = []
@@ -55,6 +106,46 @@ def parse_data(path):
                 data[-1]['persona_info'].append(persona_info)
 
         return data
+def load_dataset_3(path):
+    docs = []
+    with open(path, 'r', encoding='utf-8') as file:
+        data = []
+        sentence_counter = 0
+        persona_counter = 0
+        for line in file.readlines():
+            line = line.strip()
+
+            if len(line) == 0:
+                continue
+
+            space_idx = line.find(' ')
+            if space_idx == -1:
+                dialog_idx = int(line)
+            else:
+                dialog_idx = int(line[:space_idx])
+
+            if int(dialog_idx) == 1:
+                persona_counter += 1
+                data.append({'persona_info': [], 'dialog': []})
+
+            dialog_line = line[space_idx + 1:].split('\t')
+            dialog_line = [l.strip() for l in dialog_line]
+
+            if dialog_line[0].startswith('your persona:'):
+                persona_info = dialog_line[0].replace('your persona: ', '')
+                data[-1]['persona_info'].append(persona_info)
+                doc = {
+                    'personaId':persona_counter,
+                    'sentenceId':sentence_counter,
+                    'sentence':persona_info
+                }
+                docs.append(doc)
+
+            elif len(dialog_line) > 1:
+                data[-1]['dialog'].append(dialog_line[0])
+                data[-1]['dialog'].append(dialog_line[1])
+            sentence_counter += 1
+        return docs
 def load_dataset_2(path):
     docs = []
     count = 0
@@ -114,7 +205,7 @@ def bulk_predict(docs, batch_size=256):
 
 
 def main(args):
-    docs = load_dataset_2(args.data)
+    docs = load_dataset_3(args.data)
     with open(args.save, 'w') as f:
         for doc, emb in zip(docs, bulk_predict(docs)):
             d = create_document(doc, emb, args.index_name)
